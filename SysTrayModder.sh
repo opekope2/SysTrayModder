1ec2e4d22f36f49624069b06ad3f3ef15692477e
BuildTools <unconfigured@null.spigotmc.org>
2020-04-27 01:36:27 +0100

    Added licence to script.

--- SysTrayModder.sh
+++ SysTrayModder.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+:'
+    SysTrayModder - Modify the Plasma system tray to make it look and 
+    behave as a sidebar.
+    Copyright (C) 2020  William Franco Abdul Hai
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'
+
 ## Variables
 LOCAL_DIR=~/.local/share/plasma/plasmoids
 SYSTEM_DIR=/usr/share/plasma/plasmoids
